ac443e2f8efe94d890e2d3ceb197485f96f4dc2c
arulk081994 <arulkumar081994@gmail.com>
2025-03-02 23:33:12 +0530

    if & def sample programs

---
+++ def.py
@@ -0,0 +1,27 @@
+
+#1st Sample 
+# def arul():
+#     print("hello Arul , welocome to py")
+# arul() 
+
+
+
+#2nd sample
+# def ram():
+#     return "This is Ram from chennai"
+# native = ram()
+# print(native)
+
+#3rd program
+# def add(x,y):
+#     return x + y
+# answer = add(3,3)
+# print(answer)
+
+
+#4th program
+import keyword
+keyword.iskeyword('hello')
+
+
+
---
+++ ifstatement.py
@@ -0,0 +1,2 @@
+if 3 + 5 == 9 :
+    print("This is true ")
